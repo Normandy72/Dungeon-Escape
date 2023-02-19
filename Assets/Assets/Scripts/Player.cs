95fa3dca8d8c19e392d4bf19285307f3a12570f9
Ekaterina Slizhewskaya <ekaterina.barzdova@gmail.com>
2023-02-19 22:35:00 +0100

    add player jumping logic

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private float _jumpForce = 5.0f;
+    [SerializeField] private bool _grounded = false;
+    [SerializeField] private LayerMask _groundLayer;
+
     private Rigidbody2D _rigid;
+    private bool _resetJumpNeeded = false;    
 
     void Start()
     {
@@ -13,8 +18,54 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        PlayerMovement();
+
+        if(Input.GetKeyDown(KeyCode.Space) && _grounded == true)
+        {
+            PlayerJump();
+            _resetJumpNeeded = true;
+            StartCoroutine(ResetJumpNeededRoutine());
+        }
+
+        RaycastHit();      
+    }
+
+    private void PlayerMovement()
+    {
         // GetAxisRaw gives us only -1, 0 and 1
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        _rigid.velocity = new Vector2(horizontalInput, _rigid.velocity.y);
+        float horizontalMove = Input.GetAxisRaw("Horizontal");
+        _rigid.velocity = new Vector2(horizontalMove, _rigid.velocity.y);
+    }
+
+    private void PlayerJump()
+    {
+        _rigid.velocity = new Vector2(_rigid.velocity.x, _jumpForce);
+        _grounded = false;
+    }
+
+    private void RaycastHit()
+    {
+        // Vector2 origin, Vector2 direction, float distance, int layerMask
+        // The << operator shifts its left-hand operand left by the number of bits defined by its right-hand operand. 
+        // RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1 << 8);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.down, 0.6f, _groundLayer.value);
+        Debug.DrawRay(transform.position, Vector2.down * 0.6f, Color.green);
+
+        if(hitInfo.collider != null)
+        {
+            //Debug.Log("Hit: " + hitInfo.collider.name);
+
+            if(_resetJumpNeeded == false)
+            {
+                _grounded = true;
+            }            
+        }
+    }
+
+    IEnumerator ResetJumpNeededRoutine()
+    {
+        yield return new WaitForSeconds(0.1f);
+        _resetJumpNeeded = false;
     }
 }
